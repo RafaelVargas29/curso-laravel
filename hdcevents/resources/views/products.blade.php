2543d5d072127a0b7928af88845cf31f83fbc4fa
Rafael Vargas <79914727+RafaelVargas29@users.noreply.github.com>
2021-11-13 16:00:07 -0300

    aula#7
    Criando layout com blade

--- hdcevents/resources/views/products.blade.php
+++ hdcevents/resources/views/products.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/scripts.js"></script>
-    </head>
-    <body>
-        <h1>Tela de produtos</h1>
-        <a href="\">voltar para home</a>
-    </body>
-</html>
+@extends('layouts.main')
+@section('title', 'Produtos')
+@section('content')
+    <h1>Tela de produtos</h1>
+    <a href="\">voltar para home</a>
+@endsection
--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -1,50 +1,42 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
+@section('title',  'HDC Events')
+@section('content')
 
-        <title>Laravel</title>
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/scripts.js"></script>
-    </head>
-    <body>
-        <h1>Algum titulo</h1>
-        <img src="/img/banner.jpg" alt="Banner">
-        @if($nome == "Rafael")
-            <p>A condição é true</p>
-        @else
-            <p>A condição é false</p>
-        @endif 
+    <h1>Algum titulo</h1>
+    <img src="/img/banner.jpg" alt="Banner">
+    @if($nome == "Rafael")
+        <p>A condição é true</p>
+    @else
+        <p>A condição é false</p>
+    @endif 
+
+
+    @if($nome == "Rafael")
+        <p>O nome é Rafael!</p>
+        <p>E a sua idade é {{ $idade}}</p>
+    @else
+        <p>O nome é {{ $nome }}!</p>
+        <p>E a sua idade é {{ $idade }}</p>
+    @endif
+
+
+    @for($i = 0; $i < count($arr); $i++)
         
-        
-        @if($nome == "Rafael")
-            <p>O nome é Rafael!</p>
-            <p>E a sua idade é {{ $idade}}</p>
-        @else
-            <p>O nome é {{ $nome }}!</p>
-            <p>E a sua idade é {{ $idade }}</p>
+        <p>{{ $arr[$i] }} - {{$i}}</p>
+        @if($i == 2)
+            <p>O i é par!</p>
         @endif
-        
-        
-        @for($i = 0; $i < count($arr); $i++)
-            
-            <p>{{ $arr[$i] }} - {{$i}}</p>
-            @if($i == 2)
-                <p>O i é par!</p>
-            @endif
 
-        @endfor
+    @endfor
 
-        @foreach($arrNomes as $nome)
-            <a>{{ $loop->index }}</a>
-            <p>{{ $nome }},,</p>
-        @endforeach
+    @foreach($arrNomes as $nome)
+        <a>{{ $loop->index }}</a>
+        <p>{{ $nome }},,</p>
+    @endforeach
 
-        {{-- Espaço para códiog PHP puro--}}
-        @php
-            $name = "Rafael Vargas";
-            echo "<p>$name</p>"
-        @endphp
-    </body>
-</html>
+    {{-- Espaço para códiog PHP puro--}}
+    @php
+        $name = "Rafael Vargas";
+        echo "<p>$name</p>"
+    @endphp
+@endsection
